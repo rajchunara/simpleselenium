21926a19ac00c2dccf653525db48671457985988
rajchunara <rajchunara777@gmail.com>
2022-05-26 11:15:08 -0400

    first commit

---
+++ src/main/java/com/study/simpleselenium/App.java
@@ -0,0 +1,13 @@
+package com.study.simpleselenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
